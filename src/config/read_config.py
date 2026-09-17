3b28bdd1121f9041f15bb7a6d6b9e459c3c47e42
IvanVenkov712 <ivan.venkov.developer@gmail.com>
2026-09-17 15:00:21 +0300

    added read_config

---
+++ src/config/read_config.py
@@ -0,0 +1,25 @@
+import json
+from os import PathLike
+from pathlib import Path
+from typing import Any
+import yaml
+
+def read_yaml_file_config(path) -> dict[str, Any]:
+    with open(path, 'r', encoding='utf-8') as f:
+        return yaml.safe_load(f)
+
+def read_json_file_config(path) -> dict[str, Any]:
+    with open(path, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+def read_file_config(path) -> dict[str, Any]:
+    file_path = Path(path)
+    suff = file_path.suffix
+    if suff in ['.yaml', '.yml']:
+        return read_yaml_file_config(file_path)
+
+    elif suff in ['.json']:
+        return read_json_file_config(file_path)
+
+    else:
+        raise ValueError("Unknown configuration format")
